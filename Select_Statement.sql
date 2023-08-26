fee77de908aaeacf9dc33e40fecb0da218e1862a
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-08-26 14:38:21 +0530

    select statement

--- Select_Statement.sql
+++ Select_Statement.sql
@@ -1,2 +1,8 @@
  --Select all employee from the employee table
 Select * from tbl_employee;
+-- select one record on bais empno
+select * from tbl_employee
+where empno=1001
+-- when we want to filter the record in table by column value
+-- we need to use - where clause
+-- where clause filter the data on the basis of coulm value
