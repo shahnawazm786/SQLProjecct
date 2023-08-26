15d57a332636f5f912a2e4dee78ba4d95755001b
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-08-26 21:33:45 +0530

    insert record

--- Insert_Record_Customer_Table.sql
+++ Insert_Record_Customer_Table.sql
@@ -1,4 +1,5 @@
 use ERP_DB
 go
+-- Add new record into the Customer Table
 Insert into TBL_Customer(Cust_First_Name,Cust_Last_Name,Cust_Phone,Cust_Email)
 values('John','Fedrick','6688907453','fedricks@gmail.com')
