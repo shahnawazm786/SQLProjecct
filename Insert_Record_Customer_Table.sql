85003c2c86b636620758fcd72d2a9a58a64ed6ce
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-08-26 21:34:44 +0530

    insert record

--- Insert_Record_Customer_Table.sql
+++ Insert_Record_Customer_Table.sql
@@ -3,3 +3,7 @@ go
 -- Add new record into the Customer Table
 Insert into TBL_Customer(Cust_First_Name,Cust_Last_Name,Cust_Phone,Cust_Email)
 values('John','Fedrick','6688907453','fedricks@gmail.com')
+
+-- Add second record
+Insert into TBL_Customer(Cust_First_Name,Cust_Last_Name,Cust_Phone,Cust_Email)
+values('Mohan','Sharma','6788907450','mohan@ymail.com')
